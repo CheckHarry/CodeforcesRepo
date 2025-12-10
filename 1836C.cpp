fa1d2d00fe4aa7b6b8c39dea9eea97bc537c4714
harrycheck <checkhokan22@gmail.com>
2025-12-10 23:31:15 +0800

    commit

--- 1836C.cpp
+++ 1836C.cpp
@@ -24,7 +24,12 @@ ll pow10(ll a) {
     return res;
 }
 
-// n ^ 2 + n - 2 * k   (-1 + sqrt(1 + 8 * k)) / 2
+ll count(ll a,ll b,ll c,ll i) {
+    ll ret = min(pow10(b) - 1, pow10(c) - 1 - i) - max(pow10(b - 1), pow10(c - 1) - i) + 1;
+
+    if (1 > ret) return 0;
+    else return ret;
+}
 
 void solve(int a, int b, int c, ll k) {
     if (c < max(a,b) || c > max(a,b) + 1) {
@@ -32,32 +37,24 @@ void solve(int a, int b, int c, ll k) {
         return;
     }
 
-    if (c > max(a,b)) {
-        ll upper = (pow10(a) - pow10(a - 1)) * (pow10(b) - pow10(b - 1));
-        if (upper < k) {
-            cout << "-1\n";
-            return;
-        }
+    ll s = k;
+    ll base = pow10(a - 1);
+    ll i = -1;
+    while ((s > 0) && (base + i) < pow10(a)) {
+        i ++;
+        s = s - count(a,b,c,base + i);
+    }
 
-    
-        if (a < b) {
-            double e = (-1.0 + std::sqrt(1 + 8 * k)) / 2.0;
-            ll ee = ll(ceil(e));
-            ll rest = k - (ee * (ee - 1)) / 2;
-            cout << "ee" << " is " << ee << '\n';
-            cout << "rest" << " is " << rest << '\n';
-            ll C = pow10(c - 1) + rest - 1;
-            ll A = pow10(a - 1) - 1 + ee;
-            cout << A << " + " << C - A << " = " << C << '\n';
-        } else {
-            // 10000 90000 100000 <= 10000
-            // 10000 + 10001 + ..... + 90000 (when A = 90000)
-            // sum ( 10000 + i) i from 0 to 80000
+    if (base + i >= pow10(a)) {
+        cout << "-1\n";
+        return;
+    }
 
-            // 99001 999 100000 <= 100000 - 999
-            // 10000 + 10001 + ..... + 90000 (when A = 90000)
-            // sum ( 10000 + i) i from 0 to 80000
-        }
+    if (s == 0) {
+        cout << base + i << " + " << min(pow10(b) - 1, pow10(c) - 1 - base - i) << " = " << min(pow10(b) - 1, pow10(c) - 1 - base - i) +  base + i << '\n';
+    } else  {
+        //cout <<
+        cout << base + i << " + " << min(pow10(b) - 1, pow10(c) - 1 - base - i) + s << " = " << min(pow10(b) - 1, pow10(c) - 1 - base - i) + base + i + s << '\n';
     }
 
 }
